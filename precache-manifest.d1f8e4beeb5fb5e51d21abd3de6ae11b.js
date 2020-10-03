d0b7fda44f1bb36af7416b49b052187c97cb03db
Sreya14 <potlurisreya@gmail.com>
2020-10-03 18:16:34 +0530

    Updates

---
+++ precache-manifest.d1f8e4beeb5fb5e51d21abd3de6ae11b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5bff4a07996f1ba5158e926acdc9b9",
+    "url": "/reactData/index.html"
+  },
+  {
+    "revision": "3f9aebe1e0800118675d",
+    "url": "/reactData/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9f73784d9c8257906f8",
+    "url": "/reactData/static/js/2.3ba69d67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactData/static/js/2.3ba69d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9aebe1e0800118675d",
+    "url": "/reactData/static/js/main.0f1a9fa1.chunk.js"
+  },
+  {
+    "revision": "bcb7e93f07ca2976eace",
+    "url": "/reactData/static/js/runtime-main.c0278957.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactData/static/media/logo.5d5d9eef.svg"
+  }
+]);
